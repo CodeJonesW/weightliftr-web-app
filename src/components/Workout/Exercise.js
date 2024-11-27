6f7226d620162582b98a03de8038ecfcfef22624
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-11-26 22:16:00 -0600

    add row to workout ui

--- src/components/Workout/Exercise.js
+++ src/components/Workout/Exercise.js
@@ -1,15 +1,35 @@
 import React, { useState } from "react";
 import { Box, TextField, Typography, IconButton } from "@mui/material";
 import ControlPointIcon from "@mui/icons-material/ControlPoint";
+import ChangeCircleIcon from "@mui/icons-material/ChangeCircle";
+import RowingIcon from "@mui/icons-material/Rowing";
+import { FitnessCenter as FitnessCenterIcon } from "@mui/icons-material";
+const exerciseTypes = ["standard", "row"];
 
-const Exercise = ({ addExercise }) => {
+const Exercise = ({ addExercise, addRow }) => {
   const [reps, setReps] = useState("");
   const [sets, setSets] = useState("");
   const [name, setName] = useState("");
   const [weight, setWeight] = useState("");
+  const [rowDistance, setRowDistance] = useState("");
+  const [rowTime, setRowTime] = useState("");
+  const [exerciseTypeIndex, setExerciseTypeIndex] = useState(0);
 
   const handleAddExercise = () => {
-    addExercise({ reps, sets, name, weight });
+    switch (exerciseTypes[exerciseTypeIndex]) {
+      case "standard":
+        addExercise({ reps, sets, name, weight });
+        break;
+      case "row":
+        console.log("adding row in switch,", rowDistance, rowTime);
+        addRow({ rowDistance, rowTime });
+        break;
+      default:
+        console.log(
+          `handleAddExercise not handled ${exerciseTypes[exerciseTypeIndex]}`
+        );
+    }
+
     clearFields();
   };
 
@@ -18,78 +38,150 @@ const Exercise = ({ addExercise }) => {
     setSets("");
     setName("");
     setWeight("");
+    setRowDistance("");
+    setRowTime("");
+  };
+
+  const changeExerciseType = () => {
+    if (exerciseTypeIndex + 1 === exerciseTypes.length) {
+      setExerciseTypeIndex(0);
+    } else {
+      setExerciseTypeIndex(exerciseTypeIndex + 1);
+    }
   };
 
   return (
     <Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <TextField
-          sx={{ width: "140px" }}
-          placeholder="Reps"
-          value={reps}
-          onChange={(e) => setReps(e.target.value)}
-        />
-        <Typography
-          sx={{ paddingLeft: "8px", paddingRight: "8px" }}
-          variant="body1"
+      {exerciseTypes[exerciseTypeIndex] === "standard" ? (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
         >
-          x
-        </Typography>
-        <TextField
-          sx={{ width: "140px" }}
-          placeholder="Sets"
-          value={sets}
-          onChange={(e) => setSets(e.target.value)}
-        />
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: "8px",
-        }}
-      ></Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <TextField
-          sx={{ width: "140px" }}
-          placeholder="Exercise"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <Typography
-          sx={{ paddingLeft: "8px", paddingRight: "8px" }}
-          variant="body1"
+          <FitnessCenterIcon sx={{ paddingBottom: "16px" }} />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <TextField
+              sx={{ width: "140px" }}
+              placeholder="Reps"
+              value={reps}
+              onChange={(e) => setReps(e.target.value)}
+            />
+            <Typography
+              sx={{ paddingLeft: "8px", paddingRight: "8px" }}
+              variant="body1"
+            >
+              x
+            </Typography>
+            <TextField
+              sx={{ width: "140px" }}
+              placeholder="Sets"
+              value={sets}
+              onChange={(e) => setSets(e.target.value)}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "8px",
+            }}
+          ></Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <TextField
+              sx={{ width: "140px" }}
+              placeholder="Exercise"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Typography
+              sx={{ paddingLeft: "8px", paddingRight: "8px" }}
+              variant="body1"
+            >
+              @
+            </Typography>
+            <TextField
+              sx={{ width: "140px" }}
+              placeholder="Weight"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+            />
+          </Box>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
         >
-          @
-        </Typography>
-        <TextField
-          sx={{ width: "140px" }}
-          placeholder="Weight"
-          value={weight}
-          onChange={(e) => setWeight(e.target.value)}
-        />
-      </Box>
+          <RowingIcon sx={{ paddingBottom: "16px" }} />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <TextField
+              sx={{ width: "140px" }}
+              placeholder="Distance"
+              value={rowDistance}
+              onChange={(e) => setRowDistance(e.target.value)}
+            />
+            <Typography
+              sx={{ paddingLeft: "8px", paddingRight: "8px" }}
+              variant="body1"
+            >
+              @
+            </Typography>
+            <TextField
+              sx={{ width: "140px" }}
+              placeholder="Time"
+              value={rowTime}
+              onChange={(e) => setRowTime(e.target.value)}
+            />
+          </Box>
+        </Box>
+      )}
+
       <Box
         sx={{
           display: "flex",
-          justifyContent: "center",
+          justifyContent: "flex-end",
           paddingTop: "8px",
+        }}
+      >
+        <IconButton onClick={changeExerciseType}>
+          <ChangeCircleIcon />
+        </IconButton>
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
         }}
       >
         <IconButton onClick={handleAddExercise}>
